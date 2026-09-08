c19e015c8ccc0b824a8cfceb5e17b507740d9a83
dakshindelulu <dakshmandlekar14@gmail.com>
2026-09-08 19:12:27 +0530

    Add testbench for PISO module

--- PISO/PISO_testbenches.v
+++ PISO/PISO_testbenches.v
@@ -1,26 +1,60 @@
-module PISO( input clock , input reset , input load , input [3:0]D , output reg serial_out );
-  
-  reg [3:0]Q ;
-  
-  always@(posedge clock ) 
-    begin 
-    
-    if (reset)
-      Q<=4'b0000;
-    
-    else if (load)
-      Q<=D ;
-    
-    else begin
-      Q[3] <=1'b0 ;
-                     //,/,= 1'b0;
-    Q[2] <= Q[3];
-    Q[1] <= Q[2];
-    Q[0] <= Q[1];
+module PISO_tb;
+
+    reg clock;
+    reg reset;
+    reg load;
+    reg [3:0] D;
+
+    wire serial_out;
+
+    // DUT
+    PISO dut (
+        .clock(clock),
+        .reset(reset),
+        .load(load),
+        .D(D),
+        .serial_out(serial_out)
+    );
+
+    // Clock generation
+    always #5 clock = ~clock;
+
+    initial begin
+
+        // Waveform generation
+        $dumpfile("dump.vcd");
+        $dumpvars(0, PISO_tb);
+
+        // Initial values
+        clock = 0;
+        reset = 1;
+        load = 0;
+        D = 4'b0000;
+
+        // Reset
+        #10;
+        reset = 0;
+
+      // case 1 
+        D = 4'b1010;
+        load = 1;
+        #10;
+
+     // case 2 
+        load = 0;
+      #10;
+      
+      // case 3 
+      load = 1 ; 
+      D = 0101;
+      #10;
+      // case 4 
+      load = 0 ;
+      D = 4'b1111;
+      #10;
+
+        $finish;
+
     end
-  end 
-  
-   assign serial_out = Q[0]; 
-  
-endmodule 
-  
+
+endmodule
