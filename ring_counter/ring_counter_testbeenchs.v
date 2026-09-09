32dd35495bc40c86ceef499abf8992d64b305224
dakshmandlekar <dakshmandlekar14@gmail.com>
2026-09-09 14:32:35 +0530

    Add ring counter module and testbench files

---
+++ ring_counter/ring_counter_testbeenchs.v
@@ -0,0 +1,45 @@
+module ring_counter_tb ;
+  
+  
+  reg clock ;
+  reg reset ; 
+  
+  wire [3:0]Q ;
+  
+  ring_counter dut(
+    .clock(clock),
+    .reset(reset),
+    .Q(Q) 
+  );
+  
+  
+  
+  always #5 clock = ~clock ;
+  
+  initial begin 
+    
+    
+    $dumpfile("dump.vcd");
+$dumpvars(0, ring_counter_tb);
+    
+    // initial values 
+    
+    reset = 1 ;
+    clock =0 ;
+    
+    #10 ; 
+    
+    // deactivate reset 
+    
+    reset = 0 ; 
+    
+    
+    #100 ;
+    
+    
+    $finish ;
+    
+    
+  end 
+  
+endmodule 
